babea628a61c7692b1418157dc77d3bca4c4d897
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-24 04:57:45 -0400

    employee are displaying in table format from api

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,26 +4,35 @@ import "./Table.css"
 import TableRow from "./TableRow"
 
 function Table(){
-        // const [url, setUrl] = useState("");
-        const employees =useGet("https://randomuser.me/api/?results=250");
-        console.log(employees)
+    const employees =useGet("https://randomuser.me/api/?results=25");
+    const employeeRows = employees.map(employee => 
+            {
+                return(
+                    <tr key={employee.login.uuid}>
+                        <td>{employee.name.first}</td>
+                        <td>{employee.name.last}</td>
+                        <td>{employee.gender}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.dob.age}</td>
+                    </tr>
+                ) 
+            }
+        )
     
+
     return(
         <table>
             <thead>
                 <tr>
                     <td>First Name</td>
                     <td>Last Name</td>
-                    <td>UID</td>
                     <td>Gender</td>
                     <td>E-mail</td>
                     <td>Age</td>
                 </tr>
             </thead>
             <tbody>
-                <TableRow />
-                <TableRow />
-                <TableRow />
+                 {employeeRows}
             </tbody>
         </table>
     )
