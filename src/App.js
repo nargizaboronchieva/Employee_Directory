fbd6683ac465de550900a9719b7866fedb129e3d
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-24 04:25:52 -0400

    added Table component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/Navbar"
-
+import Navbar from "./components/Navbar";
+import Button from "./components/Button"
+import Table from "./components/Table"
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Button/>
+      <Table/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 
 import React from 'react';
-import "./Navbar.css"
+import "./Navbar.css";
+import Button from "./Button"
 
 function Navbar(){
     return(
         <div className="navbar">
             <div className="logo bold">Employee Directory</div>
-            <div className="filter">
+            <div className="descriptor">
+                <div className="bold">Sort Options: </div>
+                <Button>Name</Button>
+                <Button>UID</Button>
+                <Button>Age</Button>
+            </div>
+            <div className="descriptor">
                 <div className="bold">Filter Options: </div>
-                <div>Name</div>
-                <div></div>
+                <Button>Filter Off</Button>
+                <Button>Gender</Button>
             </div>
         </div>
     )
