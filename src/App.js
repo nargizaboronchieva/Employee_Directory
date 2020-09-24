44a9a0b5ea85e57ace0adf6414b8a83373bfc419
Nargiza Boronchieva <nargizaboronchieva@gmail.com>
2020-09-24 05:58:29 -0400

    added employee context.provider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import Button from "./components/Button"
 import Table from "./components/Table"
+import { EmployeeContext } from "./components/EmployeeContext"
 function App() {
-
- // console.log(employees)
+  const [employees, setEmployees] = useState([]);
+  const [displayedEmployees, setDisplayedEmployees] = useState([])
   return (
     <div className="App">
+     <EmployeeContext.Provider value={{employees, setEmployees, displayedEmployees, setDisplayedEmployees}}></EmployeeContext.Provider>
       <Navbar/>
       <Button/>
       <Table/>
+      </EmployeeContext.Provider>
     </div>
   );
 }
--- src/hooks/API.js
+++ src/hooks/API.js
@@ -1,52 +1,66 @@
-import {useState, useEffect} from 'react';
+import {useEffect, useContext} from 'react';
+import {EmployeeContext} from "../components/EmployeeContext"
 import axios from "axios"
 
-// Check out 20-State/03-Ins_useEffect
 export function useGet(url){
-    const [employees, setEmployees] = useState([])
+    const{employees, setEmployees, displayedEmployees, setDisplayedEmployees} = useContext(EmployeeContext)
 
-
+    // help writing custom hooks and specifically ones that are async
+    // -- https://dev.to/vinodchauhan7/react-hooks-with-async-await-1n9g
+    // -- https://reactjs.org/docs/hooks-custom.html
+    // -- TA Jackson Oppenheim Github: jacksonopp
+   
+    // gets employees from an api, stores them in both employees, and displayed employees. Only occurs once when component is mounted.
     useEffect(()=> {
         async function getEmployees(){
             try {
                 const response = await axios.get(url)
+                // using employees to store a copy of all the employees that will not be manipulated
                 setEmployees(response.data.results)
-                
-            } catch (error) {
-                console.log("error ocurred getting info from the API: ", error)
+                // using displayed employees to be a manipulated version of the employees that will be displayed for sorting and filtering.
+                setDisplayedEmployees(response.data.results)
             }
+            catch (error) {
+                console.log("error ocurred getting info from the API: ", error)
+            }   
         }
         getEmployees()
-    },[url])
+    },[])
 
-    useEffect(()=>{
+    // used to decide which sort function to trigger
+    function sortFunc(sort){
         switch(sort){
             case "name":
                 sortByName()
                 break
+            case "age":
+                sortByAge()
+                break
             default:
-                console.log("sort does not match the sort case")
+                console.log("sort does not match any cases")
         }
-    }, [sort])
-
-    function sortFunc(sort){
-        setSort(sort);
     }
 
-
-
-        function sortByName(){
-            console.log(`1.${employees[0].name.first} 2.${employees[1].name.first}`)
-            employees.sort(function(a,b){
+    // sorts the employees based on first name.
+    function sortByName(){
+         employees.sort(function(a,b){
             if(a.name.first < b.name.first){
                 return -1;
             }else{
                 return 1;
             }
         })
-
-        setEmployees(employees)
-
+        // You have to spread, because this creates a new variable, instead of just updating the variable. React will not recognize it as an update if you just update the variable.
+        setDisplayedEmployees([...employees])
     }
 
-    return {employees, sortFunc}
+    // sorts the employees based on age.
+    function sortByAge(){
+        employees.sort(function(a,b){
+            return (a.dob.age - b.dob.age)
+        })
+        setDisplayedEmployees([...employees])
+    }
+
+    return {displayedEmployees, sortFunc}
+}
